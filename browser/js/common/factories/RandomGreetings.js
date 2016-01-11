6400f21623d6971f543539538620cb85b27c3866
Joe Alves <joe@fullstackacademy.com>
2016-01-11 15:54:52 -0500

    New greeting for David :)

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -13,6 +13,7 @@ app.factory('RandomGreetings', function () {
         'This empty string is for Lindsay Levine.',
         'こんにちは、ユーザー様。',
         'Welcome. To. WEBSITE.',
+        'Hello, David!',
         'Hello, Rafi!',
         'Hello, 1511!',
         ':D',
